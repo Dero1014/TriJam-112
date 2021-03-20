8b68b6dd5122dea798b5a5901240cd7b9a87a9e7
Dero <petar.mami@gmail.com>
2021-03-20 20:42:29 +0100

    So far so good

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float Sensitivity;
+
+    Vector3 _tempCameraRot;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    
+    void Update()
+    {
+        float inputX = -(Input.GetAxisRaw("Mouse Y")) * Sensitivity * Time.deltaTime;
+;
+        float inputY = (Input.GetAxisRaw("Mouse X")) * Sensitivity * Time.deltaTime;
+
+        float cX = transform.localEulerAngles.x;
+        float cY = transform.localEulerAngles.y;
+        float cZ = transform.localEulerAngles.z;
+
+        float rotUpDown = cX + inputX;
+        float rotLeftRight = cY + inputY;  
+
+        transform.rotation = Quaternion.Euler(rotUpDown, rotLeftRight, cZ);
+
+    }
+}
---
+++ Assets/ThrowEgg.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThrowEgg : MonoBehaviour
+{
+    public GameObject Egg;
+    
+    public float ForceDelta;
+    public float MaxThrowForce;
+    public float MinThrowForce;
+
+    private Transform _arm;
+    private Slider _throwMeter;
+
+    public float _force = 0;
+
+    void Start()
+    {
+        _arm = GameObject.Find("Arm").transform;
+        _throwMeter = GameObject.Find("ThrowMeter").GetComponent<Slider>();
+    }
+
+    GameObject _currentEgg;
+    Rigidbody _eggRigi;
+    bool _noEgg = true;
+
+    void Update()
+    {
+        if (_noEgg)
+        {
+            _noEgg = false;
+            _currentEgg = Instantiate(Egg, _arm.transform.position, Quaternion.identity, _arm);
+            _eggRigi = _currentEgg.GetComponent<Rigidbody>();
+            _eggRigi.useGravity = false;
+            _eggRigi.isKinematic = true;
+        }
+
+        if (_currentEgg != null)
+            _currentEgg.transform.forward = _arm.forward;
+
+
+        if (Input.GetMouseButtonDown(0))
+            _force = MinThrowForce;
+
+        if (Input.GetMouseButton(0) && _force < MaxThrowForce)
+        {
+            _force += Time.deltaTime * ForceDelta;
+            _throwMeter.value = (_force / (MaxThrowForce - MinThrowForce)) - MinThrowForce/MaxThrowForce;
+        }
+        else if (Input.GetMouseButton(0) && _force >= MaxThrowForce)
+        {
+            _force = MaxThrowForce;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            EggThrow();
+        }
+
+
+    }
+
+    private void EggThrow()
+    {
+        _currentEgg.transform.parent = null;
+        _eggRigi.useGravity = true;
+        _eggRigi.isKinematic = false;
+        _eggRigi.AddForce(_currentEgg.transform.forward * _force * Time.deltaTime, ForceMode.Impulse);
+        _currentEgg = null;
+
+        _noEgg = true;
+    }
+}
