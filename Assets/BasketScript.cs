da99a1bdf640000bc136f5cb72407ff5e76c19fd
Dero <petar.mami@gmail.com>
2021-03-20 21:01:18 +0100

    Throwing and basketing is in

---
+++ Assets/BasketScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketScript : MonoBehaviour
+{
+    public int EggsInBasket = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Egg")
+        {
+            EggsInBasket++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
